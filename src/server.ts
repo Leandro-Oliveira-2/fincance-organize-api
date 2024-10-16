81eb63847576e39ce2d1395553faf63aa3790927
Leandro <Leandro-Oliveira-2>
2024-10-16 16:22:09 -0300

    Versão ok

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,11 @@ import prisma from './common/infra/prisma/index'; // Importação do Prisma
 
 async function startApp() {
   const chalk = (await import('chalk')).default;  // Importação dinâmica
-  await prisma.$connect();
+
 
   const title = chalk.cyan(figlet.textSync('FinanceOrganizer', { horizontalLayout: 'full' }));
   const info = chalk.yellow(`Service running at port ${env.PORT}.`);
-
+  await prisma.$connect();
   app.listen({
     host: '0.0.0.0',
     port: env.PORT,
