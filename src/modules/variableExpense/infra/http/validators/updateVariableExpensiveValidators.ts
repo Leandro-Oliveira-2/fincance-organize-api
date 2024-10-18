331009e746549ea7fbaf9c96324fb7a661d1a1e2
Leandro <Leandro-Oliveira-2>
2024-10-18 02:19:03 -0300

    Validators atualizado

--- src/modules/variableExpense/infra/http/validators/updateVariableExpensiveValidators.ts
+++ src/modules/variableExpense/infra/http/validators/updateVariableExpensiveValidators.ts
@@ -1,11 +1,11 @@
 import * as Z from 'zod';
 
 export const variableExpenseSchema = Z.object({
-  description: Z.string().min(1, "Description is required"),
-  amount: Z.number().positive("Amount must be positive"),
-  month: Z.number().min(1).max(12, "Month must be between 1 and 12"),
-  year: Z.number(),
-  category: Z.string().min(1, "Category is required"),
+  description: Z.string().min(1, "Description is required").optional(),
+  amount: Z.number().positive("Amount must be positive").optional(),
+  month: Z.number().min(1).max(12, "Month must be between 1 and 12").optional(),
+  category: Z.string().min(1, "Category is required").optional(),
+  year: Z.number().optional(),
   createdAt: Z.string().refine((date) => !isNaN(Date.parse(date)), {
     message: "Invalid date format",
   }).optional(),
